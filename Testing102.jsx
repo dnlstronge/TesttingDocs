a6ab7c47337cbb21c996e12ef10f3d1e810937ca
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-04-18 10:15:41 +0100

    Create Testing102.jsx

---
+++ Testing102.jsx
@@ -0,0 +1,21 @@
+/*
+
+LINK to DOCS: https://jestjs.io/docs/getting-started
+
+
+[1] - to use babel (required for TS)  => 
+
+npm install --save-dev babel-jest @babel/core @babel/preset-env
+
+Configure Babel to target your current version of Node by creating a babel.config.js file in the root of your project:
+
+babel.config.js
+module.exports = {
+  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+};
+
+The ideal configuration for Babel will depend on your project. See Babel's docs for more details.
+
+
+[2] - 
+*/

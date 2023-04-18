7b5fd32ae26c858a164622884cb47d00941fa8cf
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-04-18 10:03:48 +0100

    Create Testing101.tsx

---
+++ Testing101.tsx
@@ -0,0 +1,8 @@
+/* Having mixed results getting testing with jest set up so I will try to collate a step by step
+
+ [1] - to CRA a new typescript app - npx create-react-app my-app-name --template typescript
+ 
+ 
+ 
+ 
+ */

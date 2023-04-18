7c09eb94e012f082d5d28e4e9911a9ac8d6f2608
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-04-18 15:08:12 +0100

    Update someFun.ts

--- someFun.ts
+++ someFun.ts
@@ -1,1 +1,16 @@
 
+/* write a function which take two arguments, an array of codes, and an object person, check if the persons code is present in the array */
+
+
+type arry = number[]
+type obj = {
+  name: string,
+  passcode: number
+}
+
+const filterfun = (arr: arry , person: obj) => {
+/*check if daves passcode is stored in the given array*/ 
+  const remove = arr.filter()
+}
+
+filterfun([13245, 22432, 34324], {name: "dave", passcode: 12345})

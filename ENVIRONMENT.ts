d1ea00d96e984ce0f31f3460979ddce33d1eb32e
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-04-19 09:00:54 +0100

    Create ENVIRONMENT.ts

---
+++ ENVIRONMENT.ts
@@ -0,0 +1,13 @@
+/*
+Remember if testing dom elements to set environment to jsdom instead of node - 
+
+it's usually node by default so add 
+
+
+/**
+ * @jest-environment jsdom
+ */
+
+
+to top of test
+*/
